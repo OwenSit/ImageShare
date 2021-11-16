18ec01f0d8d8a887f53957dc53edcbee2edf7944
Owen Xue <owensit98@gmail.com>
2021-11-15 23:41:33 -0500

    add image from cloud

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* loading image offine image */}
+      <Image source={logo} style={{ width: 305, height: 159 }} />
+      {/* loading image on the cloud */}
+      <Image
+        source={{
+          uri: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.8JxnC_qJU3uAxMvEnXP9hgHaFj%26pid%3DApi&f=1",
+        }}
+        style={{ width: 160, height: 120 }}
+      />
       <Text
         style={{ backgroundColor: "chartreuse", color: "#888", fontSize: 18 }}
       >
